fadf31f47a81e19b8c86d4e11dd1968557038309
GladysKigen <gladys.kigen@student.moringa.com>
2023-02-16 15:49:20 +0300

    Class modification

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge
 
+#Individual students should initialize with an instance variable, @knowledge, that points to an empty array.
+#Define a method, #learn, that takes in a string and adds that string to the student's @knowledge array.
+#Define a method, #knowledge, that returns that student's knowledge array.
+    
+    def initialize knowledge = []
+        @knowledge = knowledge
+    end
+
+    def learn learn = ""
+        @learn << @knowledge
+    end
 end
---
+++ lib/user.rb
@@ -0,0 +1,4 @@
+#this is the parent or super class where the rest of the clsses will inherit from
+class User
+    attr_accessor :first_name, :last_name
+end
